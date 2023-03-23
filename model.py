788d018a09d3af8aa746b542e47a56192739dda1
Gerneve5 <milkranneurandi@gmx.net>
2023-03-23 02:00:00 -0400

    Implement part 2 of model.py

--- model.py
+++ model.py
@@ -113,3 +113,8 @@ import torch.nn as nn
 import math
 
 class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
